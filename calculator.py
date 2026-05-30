aff643bbb7044f2877885cc9da1c11dbdf0ebde7
aacharya0801-sketch <aacharya0801@gmail.com>
2026-05-29 21:45:24 -0400

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,14 @@
+def add(x,y):
+    return x+y
+def sub(x,y):
+    return x-y
+def mul(x,y):
+    return x*y
+def div(x,y):
+    return x/y
+a=int(input("Enter your number: "))
+b=int(input("Enter your number: "))
+print("SUM is : ",add(a,b))
+print("Difference is : ",sub(a,b))
+print("Product is : ",mul(a,b))
+print("Division is : ",div(a,b))
